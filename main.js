54a748acee2fe7b66149719b6be3bac2c8d0c5fc
Vishesh Handa <me@vhanda.in>
2017-02-14 18:05:32 +0100

    main.js: Accept a file from argv

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var parser = require('./parser.js');
 
-var data = fs.readFileSync('test.html').toString();
+// TODO: parse argv and get directory file list
+//       parse output
+//
 
-
+// Take notes and then convert it into markdown
+//
 // .content
 // -> This needs to be converted to markdown
 //    -> listitem
@@ -12,3 +16,28 @@ var data = fs.readFileSync('test.html').toString();
 // Attachements -
 // .attachments
 // Multiple li - check for img
+
+function generateOutputFile(note) {
+	// FIXME: Check if each item actually exists!
+	return `
+---
+title: ${note.title}
+created: ${note.date}
+tags: ${note.tags}
+archived: ${note.archived}
+---
+${note.content}
+`;
+}
+
+if (process.argv.length != 3) {
+    console.log("Usage main.js html-file");
+    process.exit(1);
+}
+
+var filePath = process.argv[2];
+var data = fs.readFileSync(filePath);
+var note = parser(data);
+
+console.log(note)
+console.log(generateOutputFile(note))
