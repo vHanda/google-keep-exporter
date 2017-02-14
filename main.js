4896c3197dae76b606bf544c9717c691b8657c12
Vishesh Handa <me@vhanda.in>
2017-02-14 21:22:33 +0100

    Serialize the note to a simple markdown file

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var parser = require('./parser.js');
+var serializer = require('./serializer.js');
 
 // TODO: parse argv and get directory file list
 //       parse output
@@ -13,22 +14,6 @@ var parser = require('./parser.js');
 // -> This needs to be converted to markdown
 //    -> listitem
 //
-// Attachements -
-// .attachments
-// Multiple li - check for img
-
-function generateOutputFile(note) {
-	// FIXME: Check if each item actually exists!
-	return `
----
-title: ${note.title}
-created: ${note.date}
-tags: ${note.tags}
-archived: ${note.archived}
----
-${note.content}
-`;
-}
 
 if (process.argv.length != 3) {
     console.log("Usage main.js html-file");
@@ -38,6 +23,10 @@ if (process.argv.length != 3) {
 var filePath = process.argv[2];
 var data = fs.readFileSync(filePath);
 var note = parser(data);
+var output = serializer.serialize(note);
 
-console.log(note)
-console.log(generateOutputFile(note))
+if (output.length == 1) {
+	var d = output[0];
+	console.log(d[0]);
+	fs.writeFileSync(d[0], d[1]);
+}
---
+++ serializer.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var uuidV4 = require('uuid/v4');
+
+/**
+ * Takes a note and serializes it into [(fileName, content)]
+ */
+var serialize = function(note) {
+    // FIXME: Serialize the attachments!
+    return [
+        [generateFilename(note), generateOutputFile(note)]
+    ];
+};
+
+
+var generateYamlFrontMatter = function(note) {
+    var lines = ['---'];
+    for (var key in note) {
+        if (!note.hasOwnProperty(key))
+            continue
+
+        if (key == 'content' || key == 'attachments')
+            continue
+
+        lines.push(key + ': ' + note[key]);
+    }
+    lines.push('---')
+
+    return lines.join("\n");
+}
+
+function generateOutputFile(note) {
+    return generateYamlFrontMatter(note) + '\n' + note.content;
+}
+
+function generateFilename(note) {
+    function sanitizeString(str) {
+        return str;
+    }
+    return sanitizeString(note.title || note.date || uuidV4()) + '.md';
+}
+
+module.exports = {
+    serialize: serialize
+};
