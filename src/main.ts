b32fcacbb612407186d1643a33b352b7e100c150
Vishesh Handa <me@vhanda.in>
2018-09-11 14:58:54 +0200

    Accept zip file as input
    This makes using this so much easier as one doesn't even need to unzip
    the google Takeout file.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as fs from "fs";
 import { parse } from "./parser";
 import { serialize } from "./serializer";
 
+import * as tmp from "tmp";
+import { promisify } from "util";
+var extractZip = require("extract-zip");
+
 // TODO: parse argv and get directory file list
 //       parse output
 //
@@ -23,11 +27,59 @@ if (process.argv.length != 4) {
 var inputDir = process.argv[2];
 var outputDir = process.argv[3];
 
-try {
-  fs.mkdirSync(outputDir);
-} catch (e) {}
+main(inputDir, outputDir);
 
-var convert = function(filePath: string, outputDir: string) {
+//
+// Input sanitation
+//
+async function main(inputDir: string, outputDir: string) {
+  if (!fs.existsSync(inputDir)) {
+    console.log("Input file does not exist");
+    process.exit(1);
+  }
+
+  var stat = fs.lstatSync(inputDir);
+  if (stat.isFile() && inputDir.endsWith(".zip")) {
+    const inputFile = inputDir;
+    inputDir = tmp.dirSync().name;
+
+    try {
+      const extract = promisify(extractZip);
+      await extract(inputFile, { dir: inputDir });
+      inputDir += "/Takeout";
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  }
+
+  var files = fs.readdirSync(inputDir);
+  if (files.indexOf("Keep") != -1 && files.indexOf("index.html") != -1) {
+    inputDir += "/Keep";
+    files = fs.readdirSync(inputDir);
+  }
+
+  //
+  // Output Sanitation
+  //
+  try {
+    fs.mkdirSync(outputDir);
+  } catch (e) {}
+
+  convertNotes(inputDir, files, outputDir);
+}
+
+function convertNotes(
+  inputDir: string,
+  inputFiles: string[],
+  outputDir: string
+) {
+  const files = inputFiles.filter(t => t.endsWith(".html"));
+  files.forEach(filePath => convertNote(inputDir + "/" + filePath, outputDir));
+  console.log("Done");
+}
+
+function convertNote(filePath: string, outputDir: string) {
   var data = fs.readFileSync(filePath).toString();
   var note = parse(data);
   if (!note) {
@@ -36,15 +88,6 @@ var convert = function(filePath: string, outputDir: string) {
   var output = serialize(note);
 
   output.forEach(out => {
-    console.log(filePath, out.fileName);
     fs.writeFileSync(outputDir + "/" + out.fileName, out.content);
   });
-};
-
-var files = fs.readdirSync(inputDir) as string[];
-if (files.indexOf("Keep") != -1 && files.indexOf("index.html") != -1) {
-  inputDir += "/Keep";
-  files = fs.readdirSync(inputDir) as string[];
 }
-files = files.filter(t => t.endsWith(".html"));
-files.forEach(filePath => convert(inputDir + "/" + filePath, outputDir));
